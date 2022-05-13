ffe2bb8bd434ab88e53450c57af34263873145c2
Jahanzeb Malik <jahanxeb.m@gmail.com>
2022-05-13 23:20:18 +0100

    commit3

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace Dice_Game
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Press any key to roll the dice!");
         }
     }
 }
