ec11988bb200c83627a78516669ae2422555c585
Jahanzeb Malik <jahanxeb.m@gmail.com>
2022-05-14 13:32:57 +0100

    diceGame3

--- Program.cs
+++ Program.cs
@@ -9,11 +9,19 @@ namespace Dice_Game
         {
 
             int playerRandomNum;
-            int enemyRandomNum;
+            int computerRandomNum;
+
+            int roundNum;
+
+            int playerWins = 0;
+            int computerWins = 0;
+            int draw = 0;
 
             Random random = new Random();
+            Console.Write("Enter the number of rounds: ");
+            roundNum = Convert.ToInt32(Console.ReadLine());
 
-            for (int i = 0; i < 10; i++)
+            for (int i = 0; i < roundNum; i++)
             {
                 Console.WriteLine("Press any key to roll the dice!");
                 Console.ReadKey();
@@ -22,9 +30,42 @@ namespace Dice_Game
 
                 Thread.Sleep(1000);
 
-                enemyRandomNum = random.Next(1, 7);
-                Console.WriteLine("Enemy rolled: " + enemyRandomNum);
-                Console.ReadKey();
+                computerRandomNum = random.Next(1, 7);
+                Console.WriteLine("Computer rolled: " + computerRandomNum);
+
+
+                if (playerRandomNum > computerRandomNum)
+                {
+                    playerWins++;
+                    Console.WriteLine("Player Score: " + playerWins + " : " + "Computer Score: " + computerWins);
+                }
+                else if (playerRandomNum < computerRandomNum)
+                {
+                    computerWins++;
+                    Console.WriteLine("Player Score: " + playerWins + " : " + "Computer Score: " + computerWins);
+                }
+                else
+                {
+                    draw++;
+                    Console.WriteLine("It's a draw");
+                }
+            }
+            Console.WriteLine("--------------");
+
+            if (playerWins > computerWins)
+            {
+                Console.WriteLine("Final Score- \r\nPlayer: " + playerWins + "\r\nComputer: " + computerWins + "\r\nDraw: " + draw);
+                Console.WriteLine("Player wins!");
+            }
+            else if (playerWins < computerWins)
+            {
+                Console.WriteLine("Final Score- \r\nPlayer: " + playerWins + "\r\ncomputer: " + computerWins + "\r\nDraw: " + draw);
+                Console.WriteLine("Computer wins!");
+            }
+            else
+            {
+                Console.WriteLine("Final Score- \r\nPlayer: " + playerWins + "\r\nComputer: " + computerWins + "\r\nDraw: " + draw);
+                Console.WriteLine("It's a draw");
             }
         }
     }
