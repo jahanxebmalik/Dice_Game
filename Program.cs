0258f6f96b6149ca25f104179bed613531517f64
Jahanzeb Malik <jahanxeb.m@gmail.com>
2022-05-14 12:54:34 +0100

    diceGame2

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace Dice_Game
 {
@@ -8,12 +9,22 @@ namespace Dice_Game
         {
 
             int playerRandomNum;
+            int enemyRandomNum;
 
             Random random = new Random();
 
             for (int i = 0; i < 10; i++)
             {
                 Console.WriteLine("Press any key to roll the dice!");
+                Console.ReadKey();
+                playerRandomNum = random.Next(1, 7);
+                Console.WriteLine("Player rolled: " + playerRandomNum);
+
+                Thread.Sleep(1000);
+
+                enemyRandomNum = random.Next(1, 7);
+                Console.WriteLine("Enemy rolled: " + enemyRandomNum);
+                Console.ReadKey();
             }
         }
     }
